3649d21c2785b7c353315026af154fbffba62f80
Jonas <jonas.jaugas@gmail.com>
2021-01-28 14:52:27 +0200

    set interval

--- JS/components/counter.js
+++ JS/components/counter.js
@@ -1,5 +1,28 @@
-function counter () {
+/*
+REIKALAVIMAI:
+- rasti vieta
+-istraukti skaiciu
+-kas sekunde ta skaiciu mazinti iki nulio
+-pasiekus nuli - parasyti FINITO
+*/
 
+
+function counter (selector) {
+    const DOM = document.querySelector(selector);
+    let number = parseFloat(DOM.innerText);
+
+    const timer = setInterval(function () {
+        showMeNumber(number);
+        number--;
+        if (number < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+
+}
+
+function showMeNumber (num) {
+    console.log(num);
 }
 
 export { counter }
